99f78eca411a2277c2de9642d25672b164528f1b
Andrew Fryer <andrew.fryer@queensu.ca>
2023-04-05 14:48:17 -0400

    Got plt.stairs to work
    https://matplotlib.org/stable/gallery/lines_bars_and_markers/stairs_demo.html

--- fuzzers/forkserver_simple/plot_coverage.py
+++ fuzzers/forkserver_simple/plot_coverage.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 import glob
 from statistics import mean, stdev
 
@@ -8,6 +9,14 @@ coverage_log_dirs = [
     "coverage_logs",
     "coverage_logs_run_1",
     "coverage_logs_run_2",
+]
+
+feedback_types = [
+    ("AflEdges", "red"),
+    ("ConstTrue", "blue"),
+    ("GrammarInput", "orange"),
+    ("GrammarOutput", "yellow"),
+    ("GrammarFull", "purple"),
 ]
 
 # afl_edges_files = glob.glob('coverage_"AflEdges"_*.csv')
@@ -26,20 +35,22 @@ def plot_files(feedback_type, color):
             # plt.plot(df.num_execs, df.coverage, label=feedback_type)
             plt.plot(df.num_execs, df.coverage, color=color, linewidth=0.5)
             # plt.scatter(df.num_execs, df.coverage, color=color)
-            coverage_at_end[feedback_type].append(df.coverage.iloc[-1])
+            final_coverage_value = df.coverage.iloc[-1]
+            coverage_at_end[feedback_type].append(final_coverage_value)
 
-plot_files("AflEdges", "red")
-plot_files("ConstTrue", "blue")
-plot_files("GrammarInput", "orange")
-plot_files("GrammarOutput", "yellow")
-plot_files("GrammarFull", "purple")
+for ft, c in feedback_types:
+    plot_files(ft, c)
 plt.xlabel("Num Execs")
 plt.ylabel("Coverage")
 # plt.legend()
 plt.savefig("coverage_plot.png")
+plt.clf()
 
 stats = {}
-for feedback_type in coverage_at_end:
+hist_vals = []
+colors = []
+labels = []
+for feedback_type, c in feedback_types:
     vals = coverage_at_end[feedback_type]
     if len(vals) > 0:
         m = mean(vals)
@@ -51,7 +62,19 @@ for feedback_type in coverage_at_end:
             "max_dev": m + 2 * s,
         }
         # print(feedback_type, mean(vals), stdev(vals))
-    # plt.scatter(vals)
+    hist_val, bins = np.histogram(vals, 10)
+    hist_vals.append(vals)
+    # hist_vals.append(hist_val)
+    colors.append(c)
+    labels.append(feedback_type)
+
+    h, edges = np.histogram(vals, bins=np.linspace(800, 1300, 50))
+
+    plt.stairs(h, edges, color=c, label=feedback_type)
+# plt.set_title("Step Histograms")
+plt.legend()
+plt.savefig("coverage_hist.png")
+
 # print(stats)
 for feedback_type in stats:
     print(feedback_type, stats[feedback_type])

8857041d8fdb8292fb6e6b63193dc044df2dad66
Andrew Fryer <andrew.fryer@queensu.ca>
2023-09-29 10:28:52 -0400

    reconfig for milcom paper setup repo (and specify rustc version)

--- fuzzers/forkserver_simple/benchmark_feedbacks.sh
+++ fuzzers/forkserver_simple/benchmark_feedbacks.sh
@@ -1,5 +1,5 @@
 for feedback_alg in "AflEdges" "ConstTrue" "GrammarInput" "GrammarOutput" "GrammarFull"; do
-    cargo rustc --release -- --cfg feedback_alg=\""${feedback_alg}"\"
+    cargo +1.70.0 rustc --release -- --cfg feedback_alg=\""${feedback_alg}"\"
     for i in {1..10}; do
         ./target/release/forkserver_simple
     done
